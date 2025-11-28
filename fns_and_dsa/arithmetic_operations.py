cb656507e9bc077902175601cc1571016dfe0879
tam-rat <tamrathabtamu1@gmail.com>
2025-11-28 22:32:40 +0300

    1

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,5 +7,7 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-          return "error: Division by zero is not allowed."
+            return "Error: Division by zero is not allowed"
         return num1 / num2
+    else:
+        return "Error: Invalid operation"
